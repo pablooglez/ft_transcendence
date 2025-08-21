ea0de661d45b73eee19daf73b187e562b68205d2
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-08-21 17:31:44 +0200

    feat: Add request path for token validation

--- auth-service/src/routes/authRoutes.ts
+++ auth-service/src/routes/authRoutes.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { registerController, loginController } from "../controllers/authController";
+import { registerController, loginController, validateController } from "../controllers/authController";
 
 export default async function authRoutes(app: FastifyInstance) {
     app.post("/login", loginController);
     app.post("/register", registerController);
+    app.post("/validate", validateController);
 }
