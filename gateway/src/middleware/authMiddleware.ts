ce241f4886e4e89c6d4d56af10772424842997e1
SalvadorChamizo <salvadorchamizo@gmail.com>
2025-09-22 20:12:40 +0200

    feat: Add verify-2fa endpoint to public urls

--- gateway/src/middleware/authMiddleware.ts
+++ gateway/src/middleware/authMiddleware.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import jwt from "jsonwebtoken";
 
-const publicUrls = ["/auth/login", "/auth/register", "/auth/refresh", "/ping"];
+const publicUrls = ["/auth/login", "/auth/register", "/auth/refresh", "/ping", "/auth/verify-2fa"];
 
 export async function authMiddleware(req: FastifyRequest, reply: FastifyReply) {  
     if (publicUrls.includes(req.url))
