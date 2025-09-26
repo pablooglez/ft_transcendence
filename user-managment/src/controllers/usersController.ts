095583bcc40581901864b9ccafed9e0321b225a9
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-09-26 17:44:19 +0200

    feat: Add email and security for userControllers
    The register controller now accept emails, and userGetter checks if the
    user is found in the database. Also, I have fix and error in userGetter,
    when returning a user as an array instead of an object

--- user-managment/src/controllers/usersController.ts
+++ user-managment/src/controllers/usersController.ts
@@ -2,13 +2,13 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { registerUser, getUser } from "../services/usersService";
 
 export async function registerController(req: FastifyRequest, reply: FastifyReply) {
-	const { username, password } = req.body as { username: string; password: string };
+   	const { username, password, email } = req.body as { username: string; password: string; email: string };
 
 	const user = await getUser(username);
 	try {
 		if (user)
 			throw new Error("Invalid username");
-		const result = await registerUser(username, password);
+		const result = await registerUser(email, username, password);
 		return reply.send(result);
 	} 
 	catch (err: any) {
@@ -21,11 +21,16 @@ export async function userGetter(req: FastifyRequest, reply: FastifyReply) {
 
 	try {
 		const user = await getUser(username);
-		return reply.send([{ 
+
+		if (!user) {
+      		return reply.code(404).send({ error: "User not found" });
+    	}
+
+		return reply.send({ 
 			id: user.id, 
 			username: user.username,
 			password: user.password
-		}]);
+		});
 	} catch (err: any) {
 		return reply.code(400).send({ error: err.message });
 	}
