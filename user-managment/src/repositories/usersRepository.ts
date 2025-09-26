9e7c5fe6eb35e1a445d416a30272d0ffbd029f54
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-09-26 17:42:51 +0200

    feat: Update createUser to store the email
    Now createUser repository function also stores the email in the database

--- user-managment/src/repositories/usersRepository.ts
+++ user-managment/src/repositories/usersRepository.ts
@@ -1,8 +1,8 @@
 import db from "../db/sqlite"
 
-export function createUser(username: string, password: string) {
-    const stmt = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-    stmt.run(username, password);
+export function createUser(email: string, username: string, password: string) {
+    const stmt = db.prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
+    stmt.run(username, password, email);
 }
 
 export function findUser(username: string) {
