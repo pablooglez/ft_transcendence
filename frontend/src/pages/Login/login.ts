ad89dfd0847bdf5ac92722f75ac2db3ca336caec
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-10-30 23:17:19 +0100

    feat: Add improvements to login logic
    I have added the following changes:
    
    - TwofaLogin page checking by argument if the user is trying to access
      it
    - Add result element checking to avoid uncaught errors in console
    - Add qrButton checking to avoid uncaught errors in console
    - Update user in localStorage after enabling 2fa
    - Hide result after changing between tabs in login page

--- frontend/src/pages/Login/login.ts
+++ frontend/src/pages/Login/login.ts
@@ -1,287 +1,24 @@
 import { getAccessToken, setAccessToken, clearAuth, isLoggedIn } from "../../state/authState"
 import { getLoginHtml, TwoFAFormHtml } from "./loginTemplate"
 import { setupLoginHandlers } from "./loginHandlers";
+import { getElement, hideElement } from "./loginDOM"
 
 export function Login(): string {
   return getLoginHtml();
 }
 
-export function TwoFALogin(): string {
-  return TwoFAFormHtml();
+export function TwoFALogin(check: number): string {
+  if (check === 1)
+    return TwoFAFormHtml();
+  else
+    location.hash = "#/login";
+  return ("");
 }
 
 export function loginHandlers(): void {
   setupLoginHandlers();
   setupLoginTabs();
 }
-
-/*export function loginHandlers() {
-  setupLoginTabs();
-  const form = document.querySelector<HTMLFormElement>("#login-form")!;
-  const result = document.querySelector<HTMLParagraphElement>("#result")!;
-  const logoutBtn = document.querySelector<HTMLButtonElement>("#logout-btn")!;
-  result.style.display = "none";
-
-  // Check if user is already logged in
-  if (isLoggedIn()) {
-    result.style.display = "block";
-    result.textContent = "✅ You are already logged in";
-    form.style.display = "none"; // hide login form
-    logoutBtn.style.display = "block"; // show logout button
-    
-    const userStr = localStorage.getItem("user");
-
-    if (userStr) {
-      const user = JSON.parse(userStr) as { id: number; username: string };
-      document.querySelector("#twofa-section")!.innerHTML = Enable2FA();
-      enable2FAHandlers(user.id, user.username);
-    }
-  }
-
-  form.onsubmit = async (e) => {
-    e.preventDefault();
-
-    const username = (document.querySelector<HTMLInputElement>("#username")!).value;
-    const password = (document.querySelector<HTMLInputElement>("#password")!).value;
-
-    try {
-      const res = await fetch("http://localhost:8080/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-        credentials: "include", // include cookies
-      });
-
-      const data = await res.json();
-
-      if (res.ok && data.accessToken) {
-        setAccessToken(data.accessToken);
-        const { id, username } = data.user;
-        localStorage.setItem("user", JSON.stringify({ id, username }));
-        result.style.display = "block";
-        result.textContent = `✅ Logged in as ${username}`;
-        form.style.display = "none";
-        logoutBtn.style.display = "block";
-        } else if (res.ok && data.requires2FA) {
-            const app = document.getElementById("app")!;
-            app.innerHTML = TwoFAForm();
-
-          const qrDiv = document.querySelector<HTMLDivElement>(".twofa-qrcode")!;
-          console.log(data);
-          try {
-            const res = await fetch("http://localhost:8080/auth/generate-qr", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${data.tempToken}`
-              },
-              body: JSON.stringify({ username: data.username, userId: data.userId }),
-              });
-  
-              const qr = await res.json();
-              if (res.ok && qr) {
-                qrDiv.innerHTML = `<img src="${qr.qr}" alt="QR Code" />`;
-              } else {
-                qrDiv.innerHTML = "❌ Failed to generate QR code";
-              }
-            } catch {
-              console.error("Failed to reach server");
-              qrDiv.innerHTML = "❌ Failed to generate QR code";
-            }
-
-            const twofaForm = document.querySelector<HTMLFormElement>("#twofa-form")!;
-            const twofaResult = document.querySelector<HTMLParagraphElement>("#twofa-result")!;
-            const twofaInput = document.querySelector<HTMLInputElement>("#twofa-code")!;
-
-            twofaForm.onsubmit = async (e) => {
-              e.preventDefault();
-              
-              const code = (document.querySelector<HTMLInputElement>("#twofa-code")!).value;
-              
-              const verifyRes = await fetch("http://localhost:8080/auth/verify-2fa", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ userId: data.userId, code }),
-                credentials: "include",
-              });
-              
-              const verifyData = await verifyRes.json();
-              if (verifyRes.ok && verifyData.accessToken) {
-                setAccessToken(verifyData.accessToken);
-              twofaResult.textContent = "✅ 2FA verified, loggen in!";
-              //twofaInput.style.display = "none";
-              //logoutBtn.style.display = "block";
-              setTimeout(() => {
-                window.location.href = "#/health";
-              }, 2000);
-              const body = document.querySelector("body")!;
-              body.style.background = "none";
-              //const html = document.querySelector("html")!;
-              //html.style.background = "none";
-              return ;
-            }
-            else {
-              twofaResult.textContent = "❌ Invalid 2FA code"
-            }
-            console.log("Nothing happens");
-          }
-      } else {
-        result.style.display = "block";
-        result.textContent = `❌ ${data.error || "Login failed"}`;
-      }
-    } catch (err) {
-      result.style.display = "block";    
-      result.textContent = "⚠️ Failed to reach server";
-    }
-  };
-
-  logoutBtn.onclick = async () => {
-    try {
-      const token = getAccessToken();
-      await fetch("http://localhost:8080/auth/logout", {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${token}`,
-        },
-        credentials: "include",
-      });
-      clearAuth();
-      result.style.display = "block";
-      result.textContent = "⚠️ Logged out successfully";
-      form.style.display = "block";
-      logoutBtn.style.display = "none";
-    } catch {
-      result.style.display = "block";
-      result.textContent = "⚠️ Failed to logout";
-    }
-  };
-}*/
-
-
-export async function autoLoginUser(username: string, password: string) {
-    setupLoginTabs();
-    const form = document.querySelector<HTMLFormElement>("#login-form")!;
-    const result = document.querySelector<HTMLParagraphElement>("#result")!;
-    const logoutBtn = document.querySelector<HTMLButtonElement>("#logout-btn")!;
-    
-    // Check if user is already logged in
-    if (isLoggedIn()) {
-      result.style.display = "block";
-      result.textContent = "✅ You are already logged in";
-      form.style.display = "none"; // hide login form
-      logoutBtn.style.display = "block"; // show logout button
-      return ;
-    }
-    try {
-      const res = await fetch("http://localhost:8080/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-        credentials: "include", // include cookies
-      });
-
-      const data = await res.json();
-
-      if (res.ok && data.accessToken) {
-        setAccessToken(data.accessToken);
-        result.style.display = "block";
-        result.textContent = `✅ Logged in as ${username}`;
-        form.style.display = "none";
-        logoutBtn.style.display = "block";
-      } else if (res.ok && data.requires2FA) {
-          document.body.innerHTML = TwoFAForm();
-
-          const qrDiv = document.querySelector<HTMLDivElement>(".twofa-qrcode")!;
-          const res = await fetch("http://localhost:8080/auth/generate-qr", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${data.tempToken}`
-            },
-            body: JSON.stringify({ username: data.username, userId: data.userId }),
-            });
-
-            const qr = await res.json();
-            if (qr) {
-              qrDiv.innerHTML = `<img src="${qr}" alt="QR Code" />`;
-            }
-
-          const twofaForm = document.querySelector<HTMLFormElement>("#twofa-form")!;
-          const twofaResult = document.querySelector<HTMLParagraphElement>("#twofa-result")!;
-
-          twofaForm.onsubmit = async (e) => {
-            e.preventDefault();
-
-            const code = (document.querySelector<HTMLInputElement>("#twofa-code")!).value;
-
-            const verifyRes = await fetch("http://localhost:8080/auth/verify-2fa", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ userId: data.userId, code }),
-              credentials: "include",
-            });
-
-            const verifyData = await verifyRes.json();
-            if (verifyRes.ok && verifyData.accessToken) {
-              setAccessToken(verifyData.accessToken);
-              twofaResult.textContent = "✅ 2FA verified, loggen in!";
-              twofaForm.style.display = "none";
-              logoutBtn.style.display = "block"
-            }
-            else {
-              twofaResult.textContent = "❌ Invalid 2FA code"
-            }
-          }
-      } else {
-        result.textContent = `❌ ${data.error || "Login failed"}`;
-      }
-    } catch (err) {
-      result.textContent = "⚠️ Failed to reach server";
-    }
-
-      logoutBtn.onclick = async () => {
-    try {
-      const token = getAccessToken();
-      await fetch("http://localhost:8080/auth/logout", {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${token}`,
-        },
-        credentials: "include",
-      });
-      clearAuth();
-      result.style.display = "block";
-      result.textContent = "⚠️ Logged out successfully";
-      form.style.display = "block";
-      logoutBtn.style.display = "none";
-    } catch {
-      result.style.display = "block";
-      result.textContent = "⚠️ Failed to logout";
-    }
-  };
-  };
-
-export async function fetchQRCode(userId: number, username: string, token: string): Promise<string | null> {
-    try {
-        const res = await fetch("http://localhost:8080/auth/generate-qr", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-          },
-          body: JSON.stringify({ username, userId }),
-          });
-
-          const data = await res.json();
-
-        if (res.ok && data.qr) {
-          return data.qr;
-        }
-        } catch {
-          console.error("Failed to reach server");
-        }
-    return null;
-  }
 
   export function TwoFAForm(): string {
     const body = document.querySelector("body")!;
@@ -344,7 +81,6 @@ export async function fetchQRCode(userId: number, username: string, token: strin
     const qrContainer = document.querySelector<HTMLDivElement>("#qr-container")!;
     const form = document.querySelector<HTMLFormElement>("#verify-2fa-form")!;
     const result = document.querySelector<HTMLParagraphElement>("#enable-2fa-result")!;
-
     enableBtn.onclick = async () => {
       try {
         const token = getAccessToken();
@@ -360,7 +96,8 @@ export async function fetchQRCode(userId: number, username: string, token: strin
          const data = await res.json();
 
          if (res.ok) {
-          result.textContent = "2FA enabled. Now click 'Generate QR' to continue."
+          if (result)
+            result.textContent = "2FA enabled. Now click 'Generate QR' to continue."
           showQRBtn.style.display = "inline-block";
           enableBtn.disabled = true;
          } else {
@@ -371,31 +108,36 @@ export async function fetchQRCode(userId: number, username: string, token: strin
       }
     };
 
-         showQRBtn.onclick = async () => {
-          try {
-            const token = getAccessToken();
-            const res = await fetch("http://localhost:8080/auth/generate-qr", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-              },
-              body: JSON.stringify({ username, userId }),
-            });
-
-            const data = await res.json();
-
-            if (res.ok && data.qr) {
-              qrContainer.innerHTML = `<img src="${data.qr}" alt="QR Code" />`;
-              form.style.display = "block";
-              result.textContent = "Scan the QR in your authentication app, then verify the code below.";
-            } else {
-              result.textContent = `${data.error || "Failed to generate the QR Code."}`;
-            }
-          } catch {
-            result.textContent = "Failed to reach server";
-          }
-        };
+        if (showQRBtn) {
+          showQRBtn.onclick = async () => {
+           try {
+             const token = getAccessToken();
+             const res = await fetch("http://localhost:8080/auth/generate-qr", {
+               method: "POST",
+               headers: {
+                 "Content-Type": "application/json",
+                 "Authorization": `Bearer ${token}`
+               },
+               body: JSON.stringify({ username, userId }),
+             });
+ 
+             const data = await res.json();
+ 
+             if (res.ok && data.qr) {
+               qrContainer.innerHTML = `<img src="${data.qr}" alt="QR Code" />`;
+               form.style.display = "block";
+              if (result)
+               result.textContent = "Scan the QR in your authentication app, then verify the code below.";
+             } else {
+                if (result)
+                 result.textContent = `${data.error || "Failed to generate the QR Code."}`;
+             }
+           } catch {
+            if (result)
+              result.textContent = "Failed to reach server";
+           }
+         };
+        }
 
     form.onsubmit = async (e) => {
       e.preventDefault();
@@ -412,15 +154,29 @@ export async function fetchQRCode(userId: number, username: string, token: strin
 
         const data = await res.json();
 
+        if (res.ok && data.success) {
+          if (result)
+            result.textContent = "2FA enabled and verified!";
+          qrContainer.style.display = "none";
+          showQRBtn.style.display = "nono";
+          form.style.display = "none";
+          const userStr = localStorage.getItem("user");
+          const user = JSON.parse(userStr!);
+          const actualUser = user.user || user;
+          actualUser.twofa = 1;
+          localStorage.removeItem("user");
+          localStorage.setItem("user", JSON.stringify(actualUser));
+          location.reload();
+        }
         if (res.ok && data.accessToken) {
           setAccessToken(data.accessToken);
-          result.textContent = "2FA enabled and verified!";
-          form.style.display = "none";
         } else {
-          result.textContent = `${data.error || "Invalid code"}`;
+          if (result)
+            result.textContent = `${data.error || "Invalid code"}`;
         }
       } catch (err) {
-        result.textContent = "Failed to reach server";
+          if (result)
+            result.textContent = "Failed to reach server";
       }
     };
   }
@@ -430,12 +186,15 @@ export function setupLoginTabs(): void {
   const tabPanes = document.querySelectorAll(".login-tab-panel");
   const tabLinkAccount = document.querySelectorAll(".login-nav-link-account");
   const tabLinkPassword = document.querySelectorAll(".login-nav-link-password");
+  const result = getElement<HTMLParagraphElement>("#result");
 
   tabLinks.forEach(link => {
     link.addEventListener("click", function (this: HTMLElement, e) {
       e.preventDefault();
       e.stopPropagation();
 
+      hideElement(result);
+      
       const target = this.getAttribute("data-tab");
       if (!target) return;
 
